b2139650eaf7aeedd62a9e1160f4910b948065f2
David <davidvaknin456@gmail.com>
2022-06-15 23:48:50 +0300

    Initial Commit

---
+++ Eventer/EventerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventerApp.swift
+//  Eventer
+//
+//  Created by David Vaknin on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct EventerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
